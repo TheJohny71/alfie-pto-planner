9397aab524fabed8d07dc84799dbff045de7ab59
TheJohny71 <jduval71@me.com>
2024-11-06 18:39:40 -0600

    Create holidayCalculator.ts

---
+++ src/utils/holidayCalculator.ts
@@ -0,0 +1,8 @@
+// File: src/utils/holidayCalculator.ts
+
+import { Holiday } from '../types/holidays';
+
+export class HolidayCalculator {
+    // [Previous HolidayCalculator code goes here]
+    // Copy the entire HolidayCalculator class from the earlier code
+}
