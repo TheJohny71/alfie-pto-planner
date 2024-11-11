43eeee30d92e9e3312a50384112733f8661971a8
TheJohny71 <jduval71@me.com>
2024-11-11 15:11:43 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/alfie-pto-planner/',
@@ -10,6 +10,16 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  css: {
+    modules: {
+      localsConvention: 'camelCase',
+    },
+    preprocessorOptions: {
+      css: {
+        charset: false
+      }
+    }
   },
   build: {
     outDir: 'dist',
