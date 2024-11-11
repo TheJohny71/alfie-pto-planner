5cd3a43debdfd58b3b8a5b725d168f3d6e801576
TheJohny71 <jduval71@me.com>
2024-11-11 14:25:11 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,25 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@styles': path.resolve(__dirname, './styles'),
+      '@assets': path.resolve(__dirname, './assets')
+    }
+  },
   base: './',
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
       input: {
-        main: './index.html',
-        calendar: './calendar.html'
+        main: path.resolve(__dirname, 'index.html'),
+        calendar: path.resolve(__dirname, 'calendar.html')
       }
     }
-  },
-  server: {
-    open: true
   }
-});
+})
