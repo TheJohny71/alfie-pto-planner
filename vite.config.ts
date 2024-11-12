37f09f1836bcee3fbfe95c1077c6d1fab774dd63
TheJohny71 <jduval71@me.com>
2024-11-12 11:28:12 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/alfie-pto-planner/', // This should match your GitHub repository name
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "./src/styles/base/variables";`
+        additionalData: `@import "/src/styles/base/_variables.scss";`
       }
     }
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-        calendar: 'calendar.html'
-      }
-    }
-  },
-  base: '/alfie-pto-planner/'
+  }
 })
