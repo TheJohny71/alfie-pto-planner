12c2e5c620bff722db14cb63786d17785b9c52dd
TheJohny71 <jduval71@me.com>
2024-11-12 16:17:47 -0600

    Update Calendar.tsx

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -1,3 +1,16 @@
+// At the top of your file
+import { CalendarState, ViewMode } from '../../types';
+
+interface CalendarProps {
+    state: CalendarState;
+    setDate: (date: Date) => void;
+    setViewMode: (mode: ViewMode) => void;
+    setRegion: (region: string) => void;
+}
+
+export const Calendar: React.FC<CalendarProps> = ({ state, setDate, setViewMode, setRegion }) => {
+    // Your component code
+}
 import React from 'react';
 import { ViewMode } from '../../types';
 import { useCalendarState } from '../../hooks/useCalendarState';
