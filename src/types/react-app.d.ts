a186eea1b24a316f4597cf836aca61e9b4aceeab
TheJohny71 <jduval71@me.com>
2024-11-12 16:14:12 -0600

    Update react-app.d.ts

--- src/types/react-app.d.ts
+++ src/types/react-app.d.ts
@@ -1,18 +1,10 @@
-/// <reference types="react" />
 /// <reference types="vite/client" />
 
-declare namespace JSX {
-  interface IntrinsicElements {
-    [elemName: string]: any;
-  }
+declare module '*.svg' {
+  import * as React from 'react';
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
 }
 
-declare module "*.css" {
-  const content: { [className: string]: string };
-  export default content;
-}
-
-declare module "*.scss" {
-  const content: { [className: string]: string };
-  export default content;
-}
+declare module 'sweetalert2';
