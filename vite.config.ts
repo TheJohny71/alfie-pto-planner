fc8a68f6a2159cba176362b002f4aedc22d2f6fb
TheJohny71 <jduval71@me.com>
2024-11-12 20:08:20 -0600

    update vite config for GitHub Pages

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/alfie-pto-planner/',  // Add this line for GitHub Pages
-  plugins: [react()],
-})
